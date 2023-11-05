2c45e466abbd37912445227cd340a7e87bc3d841
cosmobiosis <cosmobiosis@gmail.com>
2023-11-04 22:57:54 -0400

    research

--- voicegpt/test_state.py
+++ voicegpt/test_state.py
@@ -9,7 +9,6 @@ class CustomerSupportAbstractState(BaseModel):
     # )
     title: str
     description: str
-    in_current_state: bool
 
 class CustomerSupportInitialState(BaseModel):
     # title: str = Field(
@@ -18,7 +17,6 @@ class CustomerSupportInitialState(BaseModel):
     # )
     title: str =  "Asking"
     description: str = "Asking for service"
-    in_current_state: bool = False
 
 class CustomerSupportRefundState(BaseModel):
     # title: str = Field(
@@ -27,19 +25,39 @@ class CustomerSupportRefundState(BaseModel):
     # )
     title: str = "Refund"
     description: str = "Refund only after user wants to ask for service"
-    in_current_state: bool = False
 
+class CustomerSupportGetRefundReceiptState(BaseModel):
+    # title: str = Field(
+    #     default_factory=str,
+    #     description="Initial State.",
+    # )
+    title: str = "Get Refund Receipt"
+    description: str = "Can only get refund receipt after user refunds. Otherwise rejects user's requirements."
+
+class CustomerSupportRejectState(BaseModel):
+    # title: str = Field(
+    #     default_factory=str,
+    #     description="Initial State.",
+    # )
+    title: str = "reject user's requirement"
+    description: str = "When user's requirement gets rejected, changes the system to this state"
+    
 class CustomerSupportStatesGraph(BaseModel):
-    states: list[CustomerSupportAbstractState] = [CustomerSupportInitialState(), CustomerSupportRefundState()]
+    states: list[CustomerSupportAbstractState] = [
+        CustomerSupportInitialState(), 
+        CustomerSupportRefundState(),
+        CustomerSupportGetRefundReceiptState(),
+        CustomerSupportRejectState()
+    ]
 
 todo_app = AIApplication(
     state=CustomerSupportStatesGraph(),
     description=(
         "A simple customer service. Users will give instructions to perform state transition between"
-        "different customer support states"
+        "different customer support states."
     ),
 )
 
-response = todo_app("I want to ask for refund")
+response = todo_app("I want to fuck ass")
+# response = todo_app("I want to ask for a receipt of my refund")
 print(response.content)
-print(todo_app.state)
